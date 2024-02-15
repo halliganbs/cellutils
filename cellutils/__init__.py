5caf72800b81edb67b5e0881f827acbe5624a131
ben <bshalligan@gmail.com>
2024-02-15 14:03:37 -0500

    added setup and init

---
+++ cellutils/__init__.py
@@ -0,0 +1,3 @@
+from distance import euclidean, mahalanobis
+from plot import plot_plate
+from utils import split_metadata, zpad, zprime
---
+++ setup.py
@@ -0,0 +1,29 @@
+from importlib.metadata import entry_points
+from setuptools import setup
+
+import os
+
+if os.name == 'nt':
+    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='cellutils',
+    version='0.0.1',
+    description='High throughput microscopy functions',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: Number - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10'
+    ],
+    keywords='mircoscopy',
+    url="https://github.com/halliganbs/cellutils",
+    author='halliganbs',
+    author_email='bhalliga@med.umich.edu',
+    license='MIT',
+    packages=['cellutils']
+)
