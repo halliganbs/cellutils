8af1ae0ed089b4b25f5f89a6440b8159b7b6c84c
ben <bshalligan@gmail.com>
2024-02-15 14:13:17 -0500

    added make_well func

--- cellutils/utils.py
+++ cellutils/utils.py
@@ -20,3 +20,33 @@ def zprime(pcs, ncs):
     a = (3*pcs.std())+(3*ncs.std())
     b = np.abs(pcs.mean()-ncs.mean())
     return 1-(a/b)
+
+def make_well(df:pd.DataFrame, meta_cols, data_cols, id='Image_Metadata_WellID', score='score'):
+    """
+    Convert Cell level scored data into well level means
+
+    Args:
+        df (pd.DataFrame): cell lvel data
+        meta_cols (list like): List-like of metadata columns
+        data_cols (list like): List-like of data columns used in scoring
+        id (str, optional): Well ID. Defaults to 'Image_Metadata_WellID'.
+        score (str, optional): Score column name. Defaults to 'score'.
+
+    Returns:
+        pd.DataFrame: well level data
+    """
+    data = {x:[] for x in meta_cols+data_cols}
+    data[score] = []
+    data['index'] = []
+    for i, wid in enumerate(df[id].unique().tolist()):
+        temp = df.loc[df[id]==wid]
+        for c in meta_cols:
+            data[c].append(temp[c].unique()[0])
+        for c in data_cols:
+            data[c].append(temp[c].mean())
+        data[score].append(temp[score].mean())
+        data['index'] = i
+
+    well = pd.DataFrame(data=data)
+    print(well.shape)
+    return well
