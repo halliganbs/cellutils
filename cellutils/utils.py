340fba96e18e04453dcb33f2a2533ef3c41531cc
ben <bshalligan@gmail.com>
2024-02-15 13:57:10 -0500

    added zprime function

--- cellutils/utils.py
+++ cellutils/utils.py
@@ -9,3 +9,14 @@ def zpad():
 
 def split_metadata():
     print("split metadata of file name")
+    
+def zprime(pcs, ncs):
+    """
+    Zprime calculation for assay quality, value >= 0.5 are best
+    Args:
+        pcs (numpy ndarray): positive control (plateau) values (score, viability, etc)
+        ncs (numpy ndarray): negative control (baseline) values (score, viability, etc)
+    """
+    a = (3*pcs.std())+(3*ncs.std())
+    b = np.abs(pcs.mean()-ncs.mean())
+    return 1-(a/b)
