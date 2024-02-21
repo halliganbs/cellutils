5f856f3aa09230c9ecb5562984786cc85e86d122
ben <bshalligan@gmail.com>
2024-02-21 11:09:52 -0500

    updated init file

--- cellutils/__init__.py
+++ cellutils/__init__.py
@@ -1,3 +1,3 @@
-from distance import euclidean, mahalanobis
-from plot import plot_plate
-from utils import split_metadata, zpad, zprime
+from .distance import euclidean, mahalanobis
+from .plot import plot_plate
+from .utils import split_metadata, zpad, zprime
