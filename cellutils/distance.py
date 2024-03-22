55fee3aeb52edb6d7a8ea60086e867daa258bd15
bhalliga <bhalliga@med.umich.edu>
2024-03-22 12:14:07 -0400

    added mahalanobis dist from geeksforgeeks

--- cellutils/distance.py
+++ cellutils/distance.py
@@ -30,23 +30,21 @@ def euclidean(df:pd.DataFrame, data_cols, n_pcas=30):
     return euclidean_dist
         
 
-def mahalanobis(df:pd.DataFrame, data_cols, n_pcas=30):
+def mahalanobis(df:pd.DataFrame, data_cols, n_pcas=30, cov=None):
     """
+   https://www.geeksforgeeks.org/how-to-calculate-mahalanobis-distance-in-python/
     Calculate Mahalonbis distance of n number of PCA columns
     Args:
         df (pd.DataFrame): Souce data
         data_cols (list-like): list of relevant measurement columns
         n_pcas (int, optional): number of PCAs. Defaults to 30.
     """
-    data = StandardScaler().fit_transform(df[data_cols])
-    pcas = PCA(n_components=n_pcas).fit_transform(data)
+    pcas = PCA(n_components=n_pcas).fit_transform(df[data_cols])
     rows, _ = pcas.shape
-    x0 = [0 for i in range(n_pcas)]
-    mahala_dist = []
-    for i in range(rows):
-        cm = np.cov(pcas[i])
-        icm = np.linalg.inv(cm)
-        # todo fix this ^^^
-        mahala_dist.append(distance.mahalanobis(x0, pcas[i],icm))
-    
-    return mahala_dist
+    y_mu = df - np.mean(df[data_cols]) 
+    if not cov:
+        cov = np.cov(df[data_cols].values.T) 
+    inv_covmat = np.linalg.inv(cov)
+    left = np.dot(y_mu, inv_covmat)
+    mahal = np.dot(left, y_mu.T)
+    return mahal.diagonal()
