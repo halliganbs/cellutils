5f6172dcf1f500a74b4451d81f545a0710b3d09a
ben <bshalligan@gmail.com>
2024-05-09 11:52:06 -0400

    added scripts and score file

---
+++ scripts/score.py
@@ -0,0 +1,50 @@
+import os
+import re
+import glob
+import click
+import sqlite3
+import pickle
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from xgboost  import XGBRegressor
+
+from cellutils.utils import make_well
+
+@click.command()
+@click.argument('db')
+@click.argument('colname')
+@click.option('--table', '-t', default='Per_Object', required=True, help='Database table name')
+@click.option('--fname', '-f', default='Per_Object', required=True, help='Ouput file name')
+def score_plate(db, colname,  modelname, table="Per_Object", fname='Per_Object',):
+    # windows check
+    if os.name == 'nt':
+        os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+    
+    # check if files exist
+    assert os.path.exists(db), 'sqlite file does not exist'
+    assert os.path.exists(colname), 'Data column file does not exist'
+    assert os.path.exists(modelname), "Model file does not exist"
+    
+    with open(colname, 'rb') as f:
+        data_cols = pickle.load(f)
+    
+    model = XGBRegressor()
+    model.load_model(modelname)
+    
+    print(f"Reading table: {table}... ")
+    con = sqlite3.connect(db)
+    query = f"SELECT * FROM {table}"
+    dfs = []
+    for chunk in pd.read_sql_query(query, con, chunksize=int(1e6)):
+        print(f"Read chunk with size of: {len(chunk)} rows")
+        dfs.append(chunk)
+        del chunk
+    df = pd.concat(dfs)
+    
+    print("Scoring...")
+    data = StandardScaler().fit_transform(df[data_cols])
+    df['score'] = model.predict(data)
+    df.to_csv(fname+".csv", index=False)
+    
