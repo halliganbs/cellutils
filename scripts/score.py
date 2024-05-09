105cb4eed3703f683de91db6da783d4a596e0f9b
ben <bshalligan@gmail.com>
2024-05-09 11:56:50 -0400

    added logger to score

--- scripts/score.py
+++ scripts/score.py
@@ -11,13 +11,15 @@ from sklearn.preprocessing import StandardScaler
 from xgboost  import XGBRegressor
 
 from cellutils.utils import make_well
+from .logger import logger
 
 @click.command()
 @click.argument('db')
 @click.argument('colname')
 @click.option('--table', '-t', default='Per_Object', required=True, help='Database table name')
 @click.option('--fname', '-f', default='Per_Object', required=True, help='Ouput file name')
-def score_plate(db, colname,  modelname, table="Per_Object", fname='Per_Object',):
+@click.option('--log', '-l', default='score_log', required=False, help='Logfile output name')
+def score_plate(db, colname,  modelname, table, fname, log):
     # windows check
     if os.name == 'nt':
         os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -26,6 +28,8 @@ def score_plate(db, colname,  modelname, table="Per_Object", fname='Per_Object',
     assert os.path.exists(db), 'sqlite file does not exist'
     assert os.path.exists(colname), 'Data column file does not exist'
     assert os.path.exists(modelname), "Model file does not exist"
+    
+    logger(log, locals())
     
     with open(colname, 'rb') as f:
         data_cols = pickle.load(f)
