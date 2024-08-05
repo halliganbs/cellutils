732f902f44a5ee1fc60128ff3bc533fbb8c8abef
ben <bshalligan@gmail.com>
2024-08-05 15:12:54 -0400

    added extra crop functions

--- cellutils/crops.py
+++ cellutils/crops.py
@@ -5,6 +5,46 @@ import pandas as pd
 from skimage import measure, io
 from skimage.morphology import label
 
+
+# Function to create a cropped image with the cell
+def create_cropped_image(base_path, well_id, field_id_formatted, object_number, padding=5):
+    # Load the images
+    dna_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A01Z01C01.tif"
+    cmo_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A02Z01C02.tif"
+    claudin2_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A03Z01C04.tif"
+    
+    try:
+        dna_image = io.imread(dna_path)
+        cmo_image = io.imread(cmo_path)
+        claudin2_image = io.imread(claudin2_path)
+        
+        # Load the mask image to find the bounding box of the object
+        mask_path = f"Y:/CV8000/Sophia/BIS009_20240701_102850/masks/PECCU_{well_id}_T0001{field_id_formatted}L01A03Z01C03.tif" #change to your mask path
+        mask_image = io.imread(mask_path, as_gray=True)
+        labeled_mask = label(mask_image)
+        props = measure.regionprops(labeled_mask)
+        
+        for prop in props:
+            if prop.label == object_number:
+                minr, minc, maxr, maxc = prop.bbox
+                break
+        
+        # Calculate the crop boundaries with padding
+        minr = max(minr - padding, 0)
+        minc = max(minc - padding, 0)
+        maxr = min(maxr + padding, mask_image.shape[0])
+        maxc = min(maxc + padding, mask_image.shape[1])
+        
+        # Crop the images
+        cropped_dna = dna_image[minr:maxr, minc:maxc]
+        cropped_cmo = cmo_image[minr:maxr, minc:maxc]
+        cropped_claudin2 = claudin2_image[minr:maxr, minc:maxc]
+        
+        return cropped_dna, cropped_cmo, cropped_claudin2
+    except Exception as e:
+        print(f"Error creating cropped image: {e}")
+        return None, None, None
+
 # Function to find the centroid of the mask for a given ObjectNumber
 def find_centroid(mask_path, object_number):
     mask_image = io.imread(mask_path, as_gray=True)
@@ -48,3 +88,8 @@ def filter_cells(df, min_diameter=100, wellid='Image_Metadata_WellID', field='Im
             filtered_rows.append(row)
     
     return pd.DataFrame(filtered_rows)
+
+# Randomly sample 5 objects from each cluster
+def sample_objects(df, cluster_col, n_samples=5):
+    sampled_df = df.groupby(cluster_col).apply(lambda x: x.sample(n=n_samples, random_state=1)).reset_index(drop=True)
+    return sampled_df
