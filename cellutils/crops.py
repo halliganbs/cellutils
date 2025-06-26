ec336b9b35cf73318d9df1b9c9446dafdd5c06fa
ben <bshalligan@gmail.com>
2025-06-26 14:36:10 -0400

    changed cell crops to match that in UMAP-explorer

--- cellutils/crops.py
+++ cellutils/crops.py
@@ -5,91 +5,36 @@ import pandas as pd
 from skimage import measure, io
 from skimage.morphology import label
 
+import tqdm
 
-# Function to create a cropped image with the cell
-def create_cropped_image(base_path, well_id, field_id_formatted, object_number, padding=5):
-    # Load the images
-    dna_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A01Z01C01.tif"
-    cmo_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A02Z01C02.tif"
-    claudin2_path = f"{base_path}{well_id}_T0001{field_id_formatted}L01A03Z01C04.tif"
-    
-    try:
-        dna_image = io.imread(dna_path)
-        cmo_image = io.imread(cmo_path)
-        claudin2_image = io.imread(claudin2_path)
-        
-        # Load the mask image to find the bounding box of the object
-        mask_path = f"Y:/CV8000/Sophia/BIS009_20240701_102850/masks/PECCU_{well_id}_T0001{field_id_formatted}L01A03Z01C03.tif" #change to your mask path
-        mask_image = io.imread(mask_path, as_gray=True)
-        labeled_mask = label(mask_image)
-        props = measure.regionprops(labeled_mask)
-        
-        for prop in props:
-            if prop.label == object_number:
-                minr, minc, maxr, maxc = prop.bbox
-                break
-        
-        # Calculate the crop boundaries with padding
-        minr = max(minr - padding, 0)
-        minc = max(minc - padding, 0)
-        maxr = min(maxr + padding, mask_image.shape[0])
-        maxc = min(maxc + padding, mask_image.shape[1])
-        
-        # Crop the images
-        cropped_dna = dna_image[minr:maxr, minc:maxc]
-        cropped_cmo = cmo_image[minr:maxr, minc:maxc]
-        cropped_claudin2 = claudin2_image[minr:maxr, minc:maxc]
-        
-        return cropped_dna, cropped_cmo, cropped_claudin2
-    except Exception as e:
-        print(f"Error creating cropped image: {e}")
-        return None, None, None
+def crop_image(chan1, chan2, chan3, xmin, xmax, ymin, ymax):
+    crop_dna = chan1[ymin:ymax, xmin:xmax]
+    crop_cmo = chan2[ymin:ymax, xmin:xmax]
+    crop_target = chan3[ymin:ymax, xmin:xmax]
+    return np.stack((crop_dna, crop_cmo, crop_target), axis=0)
 
-# Function to find the centroid of the mask for a given ObjectNumber
-def find_centroid(mask_path, object_number):
-    mask_image = io.imread(mask_path, as_gray=True)
-    labeled_mask = label(mask_image)
-    props = measure.regionprops(labeled_mask)
-    
-    for prop in props:
-        if prop.label == object_number:
-            centroid_y, centroid_x = prop.centroid
-            return int(centroid_x), int(centroid_y)
-    return None, None
+def sample_df(df, cluster_col, n=5):
+    return df.groupby(cluster_col).apply(lambda x: x.sample(n=n, random_state=1)).reset_index(drop=True)
 
-# Function to find the diameter of the mask for a given ObjectNumber
-def find_diameter(mask_path, object_number):
-    mask_image = io.imread(mask_path, as_gray=True)
-    labeled_mask = label(mask_image)
-    props = measure.regionprops(labeled_mask)
-    
-    for prop in props:
-        if prop.label == object_number:
-            return prop.equivalent_diameter
-    return None
- 
-# Filter cells based on their diameter (remove false masks)
-def filter_cells(df, min_diameter=100, wellid='Image_Metadata_WellID', field='Image_Metadata_Field', obj_num_col='ObjectNumber'):
-    filtered_rows = []
-    
-    for index, row in df.iterrows():
-        well_id = row[wellid]
-        field_id = row[field]
-        object_number = row[obj_num_col]
+def get_crops(df, img_dir, output_dir, bbox_col,
+              chan1_col, chan2_col, chan3_col, n_sampels=5,
+              cluster_col='leiden_cluster',  bbox_area=100,
+              bbox_X_min='Cell_AreaShape_BoundingBoxMinimum_X', bbox_X_max='Cell_AreaShape_BoundingBoxMaximum_X', 
+              bbox_Y_min='Cell_AreaShape_BoundingBoxMinimum_Y', bbox_Y_max='Cell_AreaShape_BoundingBoxMaximum_Y'):
+    dt= df.loc[df[bbox_col]>bbox_area] # filter df to cells with diameter > diam_val
+    dt = sample_df(dt, cluster_col, n=n_sampels) # randomly pull n samples from each cluster group
+    for ind, row in tqdm.tqdm(dt.iterrows()):
+        cluster_id = row[cluster_col]
         
-        # Format the FieldID with 'F' prefix
-        field_id_formatted = f"F{field_id}"
+        chan1 = io.imread(os.path.join(img_dir, row[chan1_col][0]))
+        chan2 = io.imread(os.path.join(img_dir, row[chan2_col][0]))
+        chan3 = io.imread(os.path.join(img_dir, row[chan3_col][0]))
         
-        # Get the mask path, adjusting for A03 in the path
-        mask_path = f"Y:/CV8000/Sophia/BIS009_20240701_102850/masks/PECCU_{well_id}_T0001{field_id_formatted}L01A03Z01C03.tif" #change to your mask path
+        xmin = int(row[bbox_X_min])
+        xmax = int(row[bbox_X_max])
+        ymin = int(row[bbox_Y_min])
+        ymax = int(row[bbox_Y_max])
         
-        diameter = find_diameter(mask_path, object_number)
-        if diameter and diameter > min_diameter:
-            filtered_rows.append(row)
-    
-    return pd.DataFrame(filtered_rows)
-
-# Randomly sample 5 objects from each cluster
-def sample_objects(df, cluster_col, n_samples=5):
-    sampled_df = df.groupby(cluster_col).apply(lambda x: x.sample(n=n_samples, random_state=1)).reset_index(drop=True)
-    return sampled_df
+        crop_img = crop_image(chan1, chan2, chan3, xmin, xmax, ymin, ymax)
+        fname = os.path.join(output_dir, f"cluster_{cluster_id}_object_{ind}_.tif")
+        io.imsave(fname, crop_img)
